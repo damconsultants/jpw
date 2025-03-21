0f39e3279612402d9669c9a243cd9e6348ebc7f4
kuldip-ladola <kuldip1241@gmail.com>
2025-03-21 21:02:07 +0530

    chnage version from 1.0.0 to 1.0.1

--- Block/Adminhtml/System/Config/Version.php
+++ Block/Adminhtml/System/Config/Version.php
@@ -17,7 +17,7 @@ class Version extends Template implements RendererInterface
     public function render(AbstractElement $element)
     {
         /* Replace '1.0.0' with your actual module version */
-        $version = '1.0.0';
+        $version = '1.0.1';
         return '<div><strong>' . __('Version: %1', $version) . '</strong></div>';
     }
 }
